f52e9cf5f434174cfb5c7e869eb7c2d5a3816623
SimchaLeitner <benlite95@gmail.com>
2023-05-28 22:41:22 -0400

    the C# homework

---
+++ html/51/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace shirt {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] myColors = new string[] { "blue", "green ", "yellow" };
+            string[] myPatterns = new string[] { "striped", "Checkered", "Poca-Dots" };
+            foreach (string myColor in myColors) {
+                foreach (string mypattern in myPatterns) {
+                    Shirt myShirt = new Shirt(myColor,mypattern);
+                    Console.WriteLine($"{myShirt.pattern}:{ myShirt.color}");
+                }
+            }
+        }
+    }
+    internal class Shirt {
+        public string color;
+        public string pattern;
+        public Shirt(string color,string pattern) {
+            this.color = color;
+            this.pattern = pattern;
+        }
+    }
+}
