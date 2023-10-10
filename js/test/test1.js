4fbb12c0657290f866b95a250deecbd9660b8358
slubowsky <270589+slubowsky@users.noreply.github.com>
2023-10-09 22:04:27 -0400

    sl comments on tests

--- js/test/test1.js
+++ js/test/test1.js
@@ -4,6 +4,7 @@
 
     function myMap(anArray, callbackFn) {
         for (let index = 0; index < anArray.length; index++) {
+            // SL - Your replacing the values in the existing array. Question asked to leave the existing array as is, and instead return a new array with the transformed values
             anArray[index] = callbackFn(anArray[index], index, anArray);
         }
       }
@@ -12,5 +13,8 @@
       myMap(myArray, e => e + 67)
       console.log(myArray)
 
-    
-}());    
+
+}());
+
+// SL - I see the html file loads test2.js and test3.js but they dont exist... Did you just forget to commit?
+// Grade - 65????
