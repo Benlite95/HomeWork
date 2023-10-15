1b1078811cb7c59618ffe5dca02776500c66bfa4
SimchaLeitner <benlite95@gmail.com>
2023-10-15 19:49:50 -0400

    second commit

--- js/76/hw76.js
+++ js/76/hw76.js
@@ -4,12 +4,33 @@
     const addressInput = $("#address")
     const submitBtn = $("#submit")
     const contentDiv = $("#content")
+    const disableCheckbox = $("#disableCheckbox")
+
     submitBtn.on('click',addContent)
+    disableCheckbox.on('change', disableFields);
+
 
     function addContent(){
         if(nameInput.val()){
             contentDiv.append(`<h3>Name: ${nameInput.val()} | Address: ${addressInput.val()}</h3>`)
             $("input").val(null) 
         }
+        else{
+            alert("Please enter name and address before submiting")
+        }
     }
+
+
+    function disableFields() {
+        if (disableCheckbox.is(':checked')) {
+            nameInput.prop('disabled', true);
+            addressInput.prop('disabled', true);
+            submitBtn.prop('disabled', true);
+        } else {
+            nameInput.prop('disabled', false);
+            addressInput.prop('disabled', false);
+            submitBtn.prop('disabled', false);
+        }
+    }
+    
 }());  
