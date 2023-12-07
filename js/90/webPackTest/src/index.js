547d991d4b15b595e859040470d73159c821ea97
= <you@example.com>
2023-12-06 19:42:36 -0500

    second commit basic webpack

---
+++ js/90/webPackTest/src/index.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(firstName,lastName,age,address){
+        this.firstName = firstName,
+        this.lastName = lastName,
+        this.age = age,
+        this.address = address
+    }
+    print(){
+        console.log(`My name is ${this.firstName} ${this.lastName} and i am ${this.age} years old and live opn ${this.address}`)
+    }
+}
+let p = new myPerson("Elon","Musk",52,"123 Main Street")
+p.print()
